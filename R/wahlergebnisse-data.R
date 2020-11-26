3d0ed7fe5326dabbcc94f77e509e84af6652743d
Uwe Remer <uwe.remer@sowi.uni-stuttgart.de>
2020-11-26 11:47:25 +0100

    Tutorial 2 bugfix und Musterlösung Aufg S2

---
+++ R/wahlergebnisse-data.R
@@ -0,0 +1,15 @@
+#' Wahlergebnisse der Bundestagswahl 1990 bis 2017.
+#'
+#' Ausgewählte Parteien in Prozent.
+#'
+#' @docType data
+#'
+#' @usage data(wahlergebnisse)
+#'
+#' @format An object of class \code{"data.frame"}; see \code{\link[base]{data.frame}}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(wahlergebnisse)
+"wahlergebnisse"
